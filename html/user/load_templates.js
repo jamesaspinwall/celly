4bc2e47b3103c68ca09d024447a7ad51b7eb107e
James Aspinwall <jamesaspinwall@gmail.com>
2017-05-31 21:45:02 -0400

    refactor

--- html/user/load_templates.js
+++ html/user/load_templates.js
@@ -1,4 +1,4 @@
-// APP
+app.template = {}
 
 app.clear_rows = function () {
   $('table tbody tr').remove()
@@ -22,15 +22,22 @@ app.remove_row = function (id) {
 
 app.update_row = function (data) {
   $('#' + data.id + ' > td:nth-child(1)').text(data.name)
+  $('#' + data.id + ' > td:nth-child(2)').text(data.email)
 }
 
 app.edit = function (data) {
   $('#user').modal('toggle')
-  $('#name').val(data.name)
+  _.each(_.keys(_.omit(data,'id')), function (field) {
+    $('#' + field).val(data[field])
+  })
 }
 
-// TEMPLATES
-app.template = {}
+app.render_fields = function (parent, fields) {
+  _.each(fields, function (n) {
+    $(parent).append(app.template.text({id: n.name, label: n.label}))
+  })
+}
+
 app.load_row_template = function (template) {
   app.template.row = Handlebars.compile(template)
 }
@@ -38,7 +45,11 @@ app.load_row_template = function (template) {
 app.load_text_template = function (template) {
   app.template.text = Handlebars.compile(template)
   $('#search').append(app.template.text({id: 'search', label: 'Search'}))
-  $('.modal-body').append(app.template.text({id: 'name', label: 'Name'}))
+
+  // render all input fields
+  app.render_fields('.modal-body', [{name: 'name', label: 'Name'}, {name: 'email', label: 'Email'}])
+
+  // when change, all input fields will send [id,value]
   $('.form-group input').change(function () {
     var field = {}
     field[this.id] = this.value
@@ -46,11 +57,10 @@ app.load_text_template = function (template) {
   })
 }
 
+// action buttons
 $('.action').click(function () {
   server(this.id)
 })
-
-
 
 
 server('load_row_template', 'user/row_template.html')
